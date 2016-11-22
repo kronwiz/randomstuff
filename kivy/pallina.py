cb0cc4cf90b34bf1b95c587609ba79db5db65dad
Andrea Galimberti <andrea.galimberti@gmail.com>
2016-11-22 16:32:45 +0100

    Aggiunto il movimento degli ostacoli.

--- kivy/pallina.py
+++ kivy/pallina.py
@@ -5,15 +5,14 @@ from kivy.clock import Clock
 from kivy.vector import Vector
 from kivy.properties import ObjectProperty
 
-import time
-
 
 class Stage ( Widget ):
 	pass
 
 
 class Palla ( Widget ):
-	ostacolo = ObjectProperty ( None )
+	ostacolo1 = ObjectProperty ( None )
+	ostacolo2 = ObjectProperty ( None )
 
 	velocity_x = 0
 	velocity_y = -10
@@ -37,20 +36,30 @@ class Palla ( Widget ):
 
 			#anim = Animation ( x = self.x, y = 0, t = "out_bounce" )
 			#anim.start ( self )
-			self.caduta_event = Clock.schedule_interval ( self.caduta_libera, 1.0 / 60.0 )
+			self.caduta_event = Clock.schedule_interval ( self.caduta_libera, 0 ) #1.0 / 60.0 )
 
 
 	def caduta_libera ( self, dt ):
-		if ( self.y > 0 ) and ( not self.collide_widget ( self.ostacolo ) ):
+		if ( self.y > 0 ) and ( not self.collide_widget ( self.ostacolo1 ) and not self.collide_widget ( self.ostacolo2 ) ):
 			self.pos = Vector ( self.velocity_x, self.velocity_y ) + self.pos
 
 		else:
 			self.caduta_event.cancel ()
 
 
-
 class Ostacolo ( Widget ):
-	pass
+	def __init__ ( self, **kwargs ):
+		super ( Ostacolo, self ).__init__ ( **kwargs )
+
+		self.offset = 0
+		self.velocity_x = 2
+		self.move_event = Clock.schedule_interval ( self.movimento, 0 ) #1.0 / 60.0 )
+
+
+	def movimento ( self, dt ):
+		self.pos = Vector ( self.velocity_x, 0 ) + self.pos
+		self.offset += 1 if self.velocity_x > 0 else -1 
+		if ( self.offset >= 50 ) or ( self.offset <= -50 ): self.velocity_x = -1 * self.velocity_x
 
 
 class PallinaApp ( App ):
@@ -60,3 +69,4 @@ class PallinaApp ( App ):
 
 if __name__ == '__main__':
     PallinaApp ().run ()
+
