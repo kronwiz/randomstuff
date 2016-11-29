2b6a73d7e1bca02a5262d231e28aac02c7634004
Andrea Galimberti <andrea.galimberti@gmail.com>
2016-11-29 16:51:55 +0100

    Aggiunta la gestione delle altezze e un po' di personaggio.

--- kivy/scroller.py
+++ kivy/scroller.py
@@ -6,15 +6,25 @@ from kivy.vector import Vector
 from kivy.properties import ObjectProperty
 
 
+ALTEZZA = [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 3, 3, 3, 4, 5, 6, 6, 6, 6, 5, 5, 4, 3, 3, 3, 2, 1 ]
+
+
 class Blocco ( Widget ):
 	# impostata qui come attributo di classe perche' mi serve prima di aver
 	# caricato il primo blocco
 	WIDTH = 35
+	HEIGHT = 35
 
 
 class Stage ( Widget ):
 	velocita_x = -1
 	velocita_y = 0
+
+	# +1 per tenere conto dell'eventuale resto della divisione (un blocco dentro
+	# alla finestra solo in parte).
+	margine_destro = ( Window.width / Blocco.WIDTH + 1 ) * Blocco.WIDTH
+
+	altezza_corrente = 0
 
 
 	def __init__ ( self, **kwargs ):
@@ -41,9 +51,16 @@ class Stage ( Widget ):
 		for b in self.blocchi:
 			b.pos = Vector ( self.velocita_x, self.velocita_y ) + b.pos
 
-			if b.x <= Blocco.WIDTH:
-				b.x = Window.width
+			# quando il blocco e' completamente fuori dallo schermo
+			if b.x <= -1 * b.WIDTH:
+				b.x = self.margine_destro
+				b.y = ALTEZZA [ self.altezza_corrente ] * b.HEIGHT
+				self.altezza_corrente += 1
+				if self.altezza_corrente >= len ( ALTEZZA ): self.altezza_corrente = 0
 
+
+class Personaggio ( Widget ):
+	pass
 
 
 class ScrollerApp ( App ):
