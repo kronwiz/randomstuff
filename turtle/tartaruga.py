182d75dbbece0f8b3ba06bd9276d0f4b101fe929
Andrea Galimberti <andrea.galimberti@gmail.com>
2018-08-24 17:12:59 +0200

    Aggiunto il quadrato colorato con in numeri.

--- turtle/tartaruga.py
+++ turtle/tartaruga.py
@@ -22,14 +22,6 @@ for quadrato in range ( 10 ):
 done ()
 
 
-
-
-
-
-
-
-
-
 def quadrato ():
 	goto ( 0, 0 )
 	pendown ()
@@ -51,4 +43,63 @@ def triangolo ():
 	forward ( 141 )
 
 	done ()
+
+
+def quadrato_colorato_nomi ():
+	goto ( 0, 0 )
+	setheading ( 0 )
+	pendown ()
+	pencolor ( "red" )
+	forward ( 100 )
+	right ( 90 )
+	pencolor ( "orange" )
+	forward ( 100 )
+	right ( 90 )
+	pencolor ( "light green" )
+	forward ( 100 )
+	right ( 90 )
+	pencolor ( "green" )
+	forward ( 100 )
+	right ( 90 )
+
+	done ()
+
+
+def quadrato_colorato_numeri ():
+	goto ( 0, 0 )
+	setheading ( 0 )
+	pendown ()
+	pencolor ( 255, 0, 0 )
+	forward ( 100 )
+	right ( 90 )
+	pencolor ( 255, 165, 0 )
+	forward ( 100 )
+	right ( 90 )
+	pencolor ( 144, 238, 144 )
+	forward ( 100 )
+	right ( 90 )
+	pencolor ( 0, 255, 0 )
+	forward ( 100 )
+	right ( 90 )
+
+	done ()
+
+
+def stella_quadrati ():
+	goto ( 0, 0 )
+	pendown ()
+	colore = 0
+
+	for quadrato in range ( 10 ):
+		pencolor ( colore * 2, colore * 10, colore * 10 )
+
+		for lato in range ( 4 ):
+			forward ( 100 )
+			right ( 90 )
+
+		right ( 36 )
+		colore = colore + 2
+
+	done ()
+
 
